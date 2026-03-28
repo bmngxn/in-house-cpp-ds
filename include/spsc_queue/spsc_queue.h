c0dc7ba385c8a187c7b21fe6894adfc2655d46a0
bmngxn <bmnguyen0906@gmail.com>
2026-03-28 16:51:51 +0700

    add helper

--- include/spsc_queue/spsc_queue.h
+++ include/spsc_queue/spsc_queue.h
@@ -9,7 +9,7 @@ namespace bmngxn {
     concept Queueable = std::default_initializable<T> && std::move_constructible<T>;
 
     /**
-     * Lock-free SPSC queue  using a circular buffer: head as read index, tail as write index
+     * Lock-free SPSC queue  using a circular buffer: head_ as read index, tail_ as write index
      * 
      * the real cap is Capacity - 1 to distinguish between full and empty states
      */
@@ -18,9 +18,13 @@ namespace bmngxn {
     private:
         static constexpr std::size_t Capacity = 1 << N;
 
-        alignas(std::hardware_destructive_interference_size) std::atomic<std::size_t> head{0};
-        alignas(std::hardware_destructive_interference_size) std::atomic<std::size_t> tail{0}; 
+        alignas(std::hardware_destructive_interference_size) std::atomic<std::size_t> head_{0};
+        alignas(std::hardware_destructive_interference_size) std::atomic<std::size_t> tail_{0}; 
         alignas(std::hardware_destructive_interference_size) T buffer[Capacity]; 
+    
+        std::size_t next(std::size_t index) const noexcept {
+            return (index + 1) & (Capacity - 1);
+        }
     
     public:
 
@@ -41,33 +45,33 @@ namespace bmngxn {
         // perfect forwarding with variadic templates (?)
         // OR: lvalue/rvalue overloads (later)
         bool enqueue(T item) noexcept {
-            std::size_t current_tail = tail.load(std::memory_order_relaxed); // producer is the only thread modifying tail -> no need to sync with itself
-            std::size_t next_tail = (current_tail + 1) & (Capacity - 1);
+            std::size_t current_tail = tail_.load(std::memory_order_relaxed); // producer is the only thread modifying tail_ -> no need to sync with itself
+            std::size_t next_tail = next(current_tail);
 
-            if (next_tail == head.load(std::memory_order_acquire)) return false; 
+            if (next_tail == head_.load(std::memory_order_acquire)) return false; 
 
             buffer[current_tail] = std::move(item);
-            tail.store(next_tail, std::memory_order_release);
+            tail_.store(next_tail, std::memory_order_release);
             return true;
         }
 
         std::optional<T> dequeue() noexcept {
-            std::size_t current_head = head.load(std::memory_order_relaxed); // sameeeeee
+            std::size_t current_head = head_.load(std::memory_order_relaxed); // sameeeeee
 
-            if (current_head == tail.load(std::memory_order_acquire)) return std::nullopt; 
+            if (current_head == tail_.load(std::memory_order_acquire)) return std::nullopt; 
 
             T popped_item = std::move(buffer[current_head]); 
-            head.store((current_head + 1) & (Capacity - 1), std::memory_order_release);
+            head_.store(next(current_head), std::memory_order_release);
             return popped_item;
         }
 
         bool empty() const noexcept {
-            return head.load(std::memory_order_acquire) == tail.load(std::memory_order_acquire);
+            return head_.load(std::memory_order_acquire) == tail_.load(std::memory_order_acquire);
         }   
 
         bool full() const noexcept {
-            std::size_t next_tail = (tail.load(std::memory_order_acquire) + 1) & (Capacity - 1);
-            return next_tail == head.load(std::memory_order_acquire);
+            std::size_t next_tail = next(tail_.load(std::memory_order_acquire));
+            return next_tail == head_.load(std::memory_order_acquire);
         }   
 
         std::size_t capacity() const noexcept {
